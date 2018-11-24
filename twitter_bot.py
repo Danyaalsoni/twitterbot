cb86fa0a706d6ac661e655b08e65c94be997fd7a
Danyaal <danyaalsoni@gmail.com>
2018-11-24 11:41:23 -0500

    simple twitter bot

---
+++ twitter_bot.py
@@ -0,0 +1,44 @@
+
+import tweepy
+import time
+
+CONSUMER_KEY='1vTbaAaIh0zLmSkpMwAIFReDn'
+CONSUMER_SECRET='VxTbQeJj44U8c4N9FYNYKPbaFx6b4q4inHztiEHd7LFGLd0RsJ'
+ACCESS_KEY='1066136249935646720-EXOj0hUKfKa5UQQbWWZfCFo4wz04SY'
+ACCESS_SECRET='ftucA9dtTlDXRpH3AawoVDGdCfmZXpG0GYErLrRZlNjLQ'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api=tweepy.API(auth,wait_on_rate_limit=True)
+
+FILE_NAME='last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read=open(file_name, 'r')
+    last_seen_id= (f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write=open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+def reply_to_tweets():
+    print('retrieving and replying to tweets....')
+    last_seen_id=retrieve_last_seen_id(FILE_NAME)
+    mentions= api.mentions_timeline(
+                    last_seen_id,
+                    tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - '+mention.full_text)
+        last_seen_id=mention.id
+        store_last_seen_id(last_seen_id,FILE_NAME)
+        if '#helloworld' in mention.full_text.lower():
+            print('found helloworld hash')
+            print('responding back....')
+            api.update_status('@'+mention.user.screen_name + '#HelloWorld back!!!', mention.id)
+
+while True:
+    reply_to_tweets()
+    time.sleep(15)
